334dc0529d56030ccd0f123cc1bf8757bf817594
Andrian <Andrian@ND.id.au>
2024-01-10 18:50:32 +1100

    Additional interface implementations.

--- src/Pug.Effable/IReferenceInfo.cs
+++ src/Pug.Effable/IReferenceInfo.cs
@@ -12,27 +12,26 @@ namespace Pug.Effable
         /// TYpe of entity being referenced
         /// </summary>
         string Type 
-	{
-		get;
-#if NETSTANDARD2_0
-		set;
-#else
-		init;
-#endif
-	}
-        
+		{
+			get;
+	#if NETSTANDARD2_0
+			set;
+	#else
+			init;
+	#endif
+		}
         
         /// <summary>
         /// Identifier of entity being referenced
         /// </summary>
         string Value 
-	{
-		get;
-#if NETSTANDARD2_0
-		set;
-#else
-		init;
-#endif
-	}
+		{
+			get;
+	#if NETSTANDARD2_0
+			set;
+	#else
+			init;
+	#endif
+		}
     }
 }
